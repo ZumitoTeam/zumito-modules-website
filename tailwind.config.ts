60a5c1e0dbf4203b5790027e4687872ca3eec9b6
fernandomema <fernandomema@gmail.com>
2025-07-29 20:55:28 +0200

    Add tailwind-animations plugin to Tailwind CSS configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,6 @@
 import type { Config } from "tailwindcss";
+import animations from '@midudev/tailwind-animations'
+
 
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
@@ -7,5 +9,5 @@ export default {
     extend: {}
   },
 
-  plugins: [require("@tailwindcss/typography")]
+  plugins: [require("@tailwindcss/typography"), animations]
 } as Config;
