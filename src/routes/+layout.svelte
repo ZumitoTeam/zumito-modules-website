1643f6831fe6cd9935d922eb586bda29679f99bd
fernandomema <fernandomema@gmail.com>
2026-06-24 18:31:47 +0200

    Transiciones entre páginas: fade in/out en root layout con key en pathname

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,8 @@
 	import { Toaster } from 'svelte-sileo';
 	import { browser } from '$app/environment';
 	import { loadLocale } from 'wuchale/load-utils';
+	import { page } from '$app/stores';
+	import { fade } from 'svelte/transition';
 	import '../locales/main.loader.svelte.js';
 	import type { LayoutData } from './$types';
 
@@ -15,5 +17,11 @@
 
 <svelte:head><link rel="icon" href="/favicon.png" /></svelte:head>
 
-{@render children()}
+{#key $page.url.pathname}
+	<div in:fade={{ duration: 200 }} out:fade={{ duration: 150 }}>
+		{@render children()}
+	</div>
+{/key}
+
 <Toaster position="top-center" />
+
